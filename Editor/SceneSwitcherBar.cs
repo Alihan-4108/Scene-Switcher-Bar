77d9a5bb18d3a42e63201ab6c51a00a7ca7942c2
Alihan <74368418+Alihan-4108@users.noreply.github.com>
2026-07-01 01:04:56 +0300

    Update SceneSwitcherBar.cs

--- Editor/SceneSwitcherBar.cs
+++ Editor/SceneSwitcherBar.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -23,6 +24,13 @@ namespace Alihan4108.SceneSwitcherBar
         {
             var icon = EditorGUIUtility.IconContent("d_SceneAsset Icon").image as Texture2D;
 
+            if (SceneSwitcherSettings.DisplayMode == SceneSwitcherDisplayMode.Dropdown)
+            {
+                var content = new MainToolbarContent("Scenes", icon, "Open a build scene");
+                yield return new MainToolbarDropdown(content, ShowSceneMenu);
+                yield break;
+            }
+
             foreach (var guid in SceneSwitcherSettings.Selected)
             {
                 var path = AssetDatabase.GUIDToAssetPath(guid);
@@ -34,6 +42,27 @@ namespace Alihan4108.SceneSwitcherBar
 
                 yield return new MainToolbarButton(content, () => OpenScene(path));
             }
+        }
+
+        static void ShowSceneMenu(Rect rect)
+        {
+            var menu = new GenericMenu();
+
+            foreach (var guid in SceneSwitcherSettings.Selected)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+
+                if (string.IsNullOrEmpty(path)) continue;
+
+                var label = Path.GetFileNameWithoutExtension(path);
+                var scenePath = path;
+                menu.AddItem(new GUIContent(label), false, () => OpenScene(scenePath));
+            }
+
+            if (menu.GetItemCount() == 0)
+                menu.AddDisabledItem(new GUIContent("No scenes selected"), false);
+
+            menu.DropDown(rect);
         }
 
         static void OpenScene(string path)
